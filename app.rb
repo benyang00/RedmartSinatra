c31322800d84c7dc10223296fabed974ddb48a1a
ben yang <benyang00@gmail.com>
2016-07-21 08:36:51 +0800

    displayed user information retrieved from DB

---
+++ app.rb
@@ -0,0 +1,19 @@
+class RedmartSinatra < Sinatra::Base
+
+  get '/' do
+    erb 'This is the home page'
+  end
+
+  # GET /users LIST all the books
+  get '/users' do
+    @users = User.all
+    erb :'users'
+  end
+  # # GET /users/1 GET books with ID 1
+  # get '/books/:id' do
+  #   erb :'each_book'
+  #
+  # end
+  #
+
+end
---
+++ config.ru
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'bundler'
+Bundler.require
+
+require './models/user' #model
+
+require './app' #controller
+run RedmartSinatra

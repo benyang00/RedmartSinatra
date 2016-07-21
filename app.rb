7414ef801d96db51d06f1bf765373d6b7b351433
ben yang <benyang00@gmail.com>
2016-07-21 09:51:49 +0800

    added /main page

--- app.rb
+++ app.rb
@@ -9,6 +9,10 @@ class RedmartSinatra < Sinatra::Base
     @users = User.all
     erb :'users'
   end
+
+  get '/main' do
+    erb 'This is the main page.'
+  end
   # # GET /users/1 GET books with ID 1
   # get '/books/:id' do
   #   erb :'each_book'
